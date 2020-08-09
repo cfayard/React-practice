f390f416b6a847ca0380a05f24efe43351384298
Casey Fayard <fayardcasey@gmail.com>
2020-08-09 11:16:55 -0400

    arrow functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const person = {
-  name: "Casey",
-  walk() {
-    console.log(this);
-  },
-};
+// const square = function (number) {
+//   return number * number;
+// };
 
-person.walk();
+// // one param
+// const square = (number) => number * number;
+// console.log(square(5));
 
-const walk = person.walk.bind(person);
-// console.log(walk);
-walk();
-// window object reference
+// // no params
+// const square = () => {
+//   return number * number;
+// };
+
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: true },
+  { id: 3, isActive: false },
+];
+
+// const actieJobs = jobs.filter(function (job) {
+//   return job.isActive;
+// });
+// arrow version
+const actieJobs = jobs.filter((job) => job.isActive);
