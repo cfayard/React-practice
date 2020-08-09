f98bc8728442e356abc43b15a1d28886dc1894da
Casey Fayard <fayardcasey@gmail.com>
2020-08-09 12:17:03 -0400

    named and default exports

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-
-  walk() {
-    console.log("walk");
-  }
-}
-
-class Teacher extends Person {
-  constructor(name, degree) {
-    super(name);
-    this.degree = degree;
-  }
-
-  teach() {
-    console.log("teach");
-  }
-}
+import Teacher, { promote } from "./teacher";
 
 const teacher = new Teacher("casey", "hardknocks");
 
-console.log(teacher);
+// console.log(teacher);
+
+teacher.teach();
