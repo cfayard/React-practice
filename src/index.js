ad5f0a83ec658a3f608237c4afa712226bf93dff
Casey Fayard <fayardcasey@gmail.com>
2020-08-09 11:43:02 -0400

    spread operators

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-const address = {
-  street: "",
-  city: "",
-  country: "",
-};
+// spread operators
 
-// const street = address.street
-// const city = address.city
-// const country = address.country
+const first = [1, 2, 3];
+const second = [4, 5, 6];
 
-const { street, city, country } = address;
+// // old way
+// const combined = first.concat(second);
+
+// // spread op way
+// // makes it possibleto add new items as well
+// const combined = [...first, "a", ...second, "b"];
+
+const clone = [...first];
+console.log(clone);
+console.log(first);
