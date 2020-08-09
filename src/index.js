ce9d10312d13ed8d175a35e6a4bb7402b48e0b1b
Casey Fayard <fayardcasey@gmail.com>
2020-08-09 11:26:05 -0400

    arrow function don't rebind this

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
-// const square = function (number) {
-//   return number * number;
-// };
+const person = {
+  talk() {
+    setTimeout(() => {
+      console.log("this", this);
+    }, 1000);
+  },
+};
 
-// // one param
-// const square = (number) => number * number;
-// console.log(square(5));
-
-// // no params
-// const square = () => {
-//   return number * number;
-// };
-
-const jobs = [
-  { id: 1, isActive: true },
-  { id: 2, isActive: true },
-  { id: 3, isActive: false },
-];
-
-// const actieJobs = jobs.filter(function (job) {
-//   return job.isActive;
-// });
-// arrow version
-const actieJobs = jobs.filter((job) => job.isActive);
+person.talk();
