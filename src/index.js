5fc6c64b48e41e170b3a0b56def07b492de28afc
Casey Fayard <fayardcasey@gmail.com>
2020-08-09 11:56:48 -0400

    classes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-// spread operators
 
-const first = [1, 2, 3];
-const second = [4, 5, 6];
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
 
-// // old way
-// const combined = first.concat(second);
+  walk() {
+    console.log("walk");
+  }
+}
 
-// // spread op way
-// // makes it possibleto add new items as well
-// const combined = [...first, "a", ...second, "b"];
-
-const clone = [...first];
-console.log(clone);
-console.log(first);
+const person = new Person('casey')
+person.
